691375c781688546b9c335e3d1aab4bce3887975
Guillaume <197660339+Touky13@users.noreply.github.com>
2025-04-02 19:43:05 +0200

    Added custom cover logic

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const myLibrary = [];
 
-function Book(title, author, length, status, id) {
+function Book(title, author, length, status, cover, id) {
   // the constructor...
   this.title = title;
   this.author = author;
   this.length = length;
   this.status = status;
+  this.cover = cover;
   this.id = id;
 }
 
-function addBookToLibrary(title, author, length, status, id) {
+function addBookToLibrary(title, author, length, status, cover, id) {
   // take params, create a book then store it in the array
-  let book = new Book(title, author, length, status, id);
+  let book = new Book(title, author, length, status, cover, id);
   return myLibrary.push(book);
 }
 
-addBookToLibrary("test", "John", "32 pages", "read", crypto.randomUUID());
-addBookToLibrary("test2", "Marc", "50 pages", "not read", crypto.randomUUID());
-addBookToLibrary("test3", "Alice", "74 pages", "read", crypto.randomUUID());
+addBookToLibrary("test", "John", "32 pages", "read", "", crypto.randomUUID());
+addBookToLibrary("test2", "Marc", "50 pages", "not read", "", crypto.randomUUID());
+addBookToLibrary("test3", "Alice", "74 pages", "read", "", crypto.randomUUID());
 console.log(myLibrary);
  
 
@@ -26,7 +27,10 @@ myLibrary.forEach (book => {
   div.classList.add("card");
   document.querySelector(".main").appendChild(div);
   let img = document.createElement("img");
+  if (book.cover === '') {
   img.src = 'assets/book-cover-placeholder-hd.png';
+  }
+  else { img.src = book.cover };
   div.appendChild(img);
   let h3 = document.createElement("h3");
   h3.setAttribute("id", "title");
